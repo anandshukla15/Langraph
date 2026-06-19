aeebdc368ffde6474deaccccb9f5c9d3dccc9aeb
anandshukla15 <anandshukla1742@gmail.com>
2026-06-19 17:37:15 +0530

    ne on eadded

---
+++ langraph_frontend.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+message_history=[]
+for message in message_history:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input=st.chat_input('Type here') 
+
+if user_input:
+
+    message_history.append({'role':'user','content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+
+    message_history.append({'role':'assistant','content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)    
