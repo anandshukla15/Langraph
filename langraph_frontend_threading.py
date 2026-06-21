9161812aa5312967d0646c7f60c0282923218901
anandshukla15 <anandshukla1742@gmail.com>
2026-06-21 21:44:43 +0530

    adding thread id

--- langraph_frontend_threading.py
+++ langraph_frontend_threading.py
@@ -4,15 +4,37 @@ from langchain_core.messages import HumanMessage
 #session state=store message 
 import uuid
 
+#utility finvction
+def generate_thread_id():
+    thread_id=uuid.uuid4()
+    return thread_id
 
-CONFIG={'configurable':{'thread_id':'thread-1'}}
+def reset_chat():
+    thread_id=generate_thread_id()
+    st.session_state['thread_id']=thread_id
+    st.session_state['message_history']=[]
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id);
+##sessin state
+
 if 'message_history' not in st.session_state:
     st.session_state['message_history']=[]
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id']=generate_thread_id()
 
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads']=[]
+
+#sidebar ui
 st.sidebar.title('Langraph chatbot')
-st.sidebar.button('new chat');
-st.sidebar.header('myconverstation')    
+if st.sidebar.button('new chat'):
+    reset_chat()
+
+st.sidebar.header('myconverstation')  
+st.sidebar.text(st.session_state['thread_id'])  
 
 
 for message in st.session_state['message_history']:
@@ -27,7 +49,7 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    
+    CONFIG={'configurable':{'thread_id': st.session_state['thread_id']}}
 
     # response=chatbot.invoke({"messages": [HumanMessage(content=user_input)]},config=CONFIG)
     # ai_message=response['messages'][-1].content
@@ -37,7 +59,7 @@ if user_input:
         ai_message=st.write_stream(
             message_chunk.content for message_chunk,metadata in chatbot.stream(
                 {'messages':[HumanMessage(content=user_input)]},
-                config={'configurable':{'thread_id':'thread-1'}},
+                config=CONFIG,
                 stream_mode='messages'
             )
         )
