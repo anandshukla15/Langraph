42c4e6f363958614e93eb19824feaed686af4a34
anandshukla15 <anandshukla1742@gmail.com>
2026-06-22 23:39:44 +0530

    done with backend

---
+++ langraph_backend_database.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.sqlite import SqliteSaver
+##from langgraph.checkpoint import Checkpoint 
+from dotenv import load_dotenv
+from langgraph.graph.message import add_messages
+import sqlite3
+
+
+load_dotenv()
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage],add_messages]
+
+
+def chat_node(state:ChatState):
+    messages=state["messages"]
+    response=llm.invoke(messages)
+    return {"messages":[response]}
+
+
+conn=sqlite3.connect(database='chatbot.db', check_same_thread=False)
+checkpointer=SqliteSaver(conn=conn)
+
+graph=StateGraph(ChatState)
+graph.add_node("chat_node",chat_node)
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
+
+chatbot=graph.compile(checkpointer=checkpointer)
